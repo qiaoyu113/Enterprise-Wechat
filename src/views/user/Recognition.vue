5f488f35c0fd4b48d8376a762d94bb67acbe3f77
qiaoyu113 <527324363@qq.com>
2020-04-01 11:27:41 +0800

    test

--- src/views/user/Recognition.vue
+++ src/views/user/Recognition.vue
@@ -73,6 +73,7 @@ export default {
       }).then((res) => {
         if (res.data.success) {
           let data = res.data.data;
+          alert('getCorpSignature' + JSON.stringify(data))
           wx.config({
             beta: true,
             debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
@@ -92,7 +93,6 @@ export default {
               ],
               success: function(res) {
                 console.log('checkJsApi', res)
-                alert('getAgentSignature' + that.GLOBAL.agentId)
                 getAgentSignature({
                   agentId: that.GLOBAL.agentId,
                   url: hostName
