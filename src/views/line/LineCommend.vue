b7312471f95bf176ec04da2e07cc4eea51ac5b3d
qiaoyu113 <527324363@qq.com>
2020-04-01 11:42:37 +0800

    test

--- src/views/line/LineCommend.vue
+++ src/views/line/LineCommend.vue
@@ -93,7 +93,7 @@ export default {
           let data = res.data.data;
           wx.config({
             beta: true,
-            debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+            debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
             appId: data.corpId, // 必填，企业号的唯一标识，此处填写企业号corpid
             timestamp: Number(data.timestamp), // 必填，生成签名的时间戳
             nonceStr: data.nonceStr, // 必填，生成签名的随机串
