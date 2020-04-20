d2ec45ec3249dcd566223a2e681640cf9186c047
qiaoyu113 <527324363@qq.com>
2020-04-20 18:55:06 +0800

    qiaoyu push

--- src/views/driver/DriverFollowDetail.vue
+++ src/views/driver/DriverFollowDetail.vue
@@ -57,7 +57,7 @@
         </div>
       </div>
       <div v-if="params.remark" class="list-note">
-        <p>{{ params.remark }}</p>
+        {{ params.remark }}
       </div>
     </div>
     <div v-if="list.length > 0" class="speed">
@@ -332,11 +332,6 @@ $danger: #E75E60;
           position: absolute;
           right: 0;
           top: 0;
-          height: 20px;
-          line-height: 20px;
-          font-size: 13px;
-          color: #FFFFFF;
-          border-radius: 5px;
         }
         .line-tit{
           font-size: 16px;
