9e64610763b7345c07c137d7f4f837c15dbbb861
qiaoyu <qiaoyu@yunniao.me>
2020-05-19 20:01:38 +0800

    add actions list

--- src/views/driver/DriverDetail.vue
+++ src/views/driver/DriverDetail.vue
@@ -299,6 +299,7 @@ export default {
         { name: '激活推送', color: '#3F8AF2' },
         { name: '产品介绍', color: '#3F8AF2' },
         { name: '推荐线路', color: '#3F8AF2' }
+        // { name: '重新匹配', color: '#D03228' }
       ],
       carType: [],
       cargoType: [],
