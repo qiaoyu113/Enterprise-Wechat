d9fc652f732bf009a4c041d4a1a90e21239337bd
qiaoyu <qiaoyu@yunniao.me>
2020-05-20 09:41:15 +0800

    none data question

--- src/components/Global.vue
+++ src/components/Global.vue
@@ -2,7 +2,7 @@
 let gio = window.gio;
 let agentId = '1000016';
 let url = window.location.host
-let version = 'v1.0.5'
+let version = 'v1.0.6'
 if (url !== 'firmiana-wechat.yunniao.cn') {
   if (url !== 'firmiana-wechat.m1.yunniao.cn') {
     // url = 'http://firmiana-wechat.m1.yunniao.cn/'
